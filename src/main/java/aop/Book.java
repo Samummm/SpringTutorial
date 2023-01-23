ddbf8674a84928490e095aea5da9d480d2a1ef20
Mad Max <samumm@mail.ru>
2023-01-23 18:00:43 +0300

    2.8

--- src/main/java/aop/Book.java
+++ src/main/java/aop/Book.java
@@ -7,8 +7,21 @@ import org.springframework.stereotype.Component;
 public class Book {
     @Value("Тайна двух океанов")
     private String name;
+    @Value("Г.Б.Адамов")
+    private String author;
+
+    @Value("1938")
+    private int yeasOfPublication;
 
     public String getName() {
         return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYeasOfPublication() {
+        return yeasOfPublication;
     }
 }
